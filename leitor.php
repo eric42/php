4f8681f8b44d72ffcc39ebdf8c5cab3131517ed4
eric42 <eric_eumesmo@yahoo.com.br>
2017-10-27 13:43:10 -0200

    sistema de leitura de arquivos

--- leitor.php
+++ leitor.php
@@ -22,6 +22,26 @@ class Csv
 
 }
 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<form action="leitor.php" method="post" enctype="multipart/form-data">
+<label>Arquivo:</label>
+<input type="file" name="arquivo" />
+
+<input type="submit" name="Enviar" /   >
+<?php
+
 $csv = new Csv('arquivo.csv');  
 $conteudo = $csv->abre();  
+
+
 ?>
+</form>
+</body>
+</html>
